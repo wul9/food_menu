58862f288bdb4fd8cc146656378a4d1122b7ac2e
wul9 <96085412+wul9@users.noreply.github.com>
2022-11-14 00:05:28 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ void main() {
 class Menu extends StatelessWidget {
   const Menu({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
